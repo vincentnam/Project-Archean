a52863105f767827555b0681a747005fdb111b42
Vincent-Nam DANG <dang.vincentnam@gmail.com>
2019-10-28 15:24:16 +0100

    extraction data annotees / selection selon le medium

--- test.py
+++ test.py
@@ -11,4 +11,21 @@ for index, row in csv_file.iterrows():
 
 only_commented = pd.DataFrame(only_commented)
 
+print(only_commented)
 
+# only_commented.to_csv("./commented_annotations.csv")
+
+# Audio / Audio vidéo / Audio texte / Texte / Audio Video Texte
+
+def get_medium(medium, df):
+    return df[df["code_doc"].str.contains(".*"+medium+"_[0-1]{1}",regex=True)]
+print("Audio")
+print(get_medium("100",only_commented))
+print("Texte")
+print(get_medium("001",only_commented))
+print("Audio vidéo")
+print(get_medium("110",only_commented))
+print("Audio texte")
+print(get_medium("101",only_commented))
+print("All")
+print(get_medium("111",only_commented))
